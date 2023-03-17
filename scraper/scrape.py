bb75c1e65612acb3bd2c9a6da408a4d460c63ffa
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-03-17 00:46:10 +0000

    I got the urls!

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -8,12 +8,19 @@ html = response.content
 
 soup = BeautifulSoup(html, features="html.parser")
 ul = soup.find_all('ul')[-1]
+list_of_links = []
+list_of_dates = []
 for li in ul.find_all('li'):
-    for a in li.find_all('a'):
-        list_of_links = []
-        for link in a.find('href'):
-            list_of_links.append("https://www.greenbeltmd.gov" + link.find('href'))
-print(list_of_links)
+    if li.find('a'):
+        list_of_links.append("https://www.greenbeltmd.gov" + li.find('a')['href'])
+        list_of_dates
+list_of_dates = []
+
+
+
+
+
+#print(list_of_links)
 
         
         
