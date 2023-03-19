48b6905221470332fe3ea091b3b12a32c61805c9
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-03-19 22:41:42 +0000

    maybe this is closer?

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -10,18 +10,22 @@ html = response.content
 soup = BeautifulSoup(html, features="html.parser")
 ul = soup.find_all('ul')[-1]
 
+#set up a container for all my records
+list_of_rows = []
+#start my loop
 for li in ul.find_all('li'):
     for a in li.find_all('a'):
-        list_of_dates = []
+        each_row = []
         if li.find('a'):
-            list_of_dates.append(li.text)
-        list_of_urls = []
-        if li.find('a'):
-            list_of_urls.append("https://www.greenbeltmd.gov" + li.find('a')['href'])
-            list_of_dates.append(list_of_urls)
+            print(li.text, "https://www.greenbeltmd.gov" + li.find('a')['href'])
+    list_of_rows.append(each_row)
+    print(list_of_rows)
+            
+
+
         
             
-        outfile = open('./reports.csv', 'w')
-        writer = csv.writer(outfile)
-        writer.writerows(list_of_dates)
+#outfile = open('./reports.csv', 'w')
+#writer = csv.writer(outfile)
+#writer.writerows(list_of_rows)
 
