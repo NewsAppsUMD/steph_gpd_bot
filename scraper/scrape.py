9cf1bb3e0ed4edc64e5482ba1d328419354740de
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-03-19 00:27:16 +0000

    i have dates and urls!

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -11,16 +11,26 @@ soup = BeautifulSoup(html, features="html.parser")
 ul = soup.find_all('ul')[-1]
 
 list_of_rows = []
-for row in ul.find_all('li'):
-    list_of_cells = []
-    for cell in row.find_all('a'):
-        if cell.find('a'):
-            list_of_cells.append(cell.text)
-            list_of_cells.append("https://www.greenbeltmd.gov" + cell.find('a')['href'])
-    text = cell.text.strip()
-    list_of_cells.append(text)
-list_of_rows.append(list_of_cells)
+for li in ul.find_all('li'):
+    for a in li.find_all('a'):
+        if li.find('a'):
+            list_of_rows.append(li.text)
+            list_of_rows.append("https://www.greenbeltmd.gov" + li.find('a')['href'])
 print(list_of_rows)
+
+#list_of_rows = []
+#for row in ul.find_all('li'):
+    #list_of_cells = []
+    #for cell in row.find_all('a'):
+        #if cell.find('a'):
+            #list_of_cells.append("https://www.greenbeltmd.gov" + cell.find('a')['href'])
+    #print(list_of_cells)
+        #if cell.find(text)
+            #list_of_cells.append(cell.text)
+    #text = cell.text.strip()
+    #list_of_cells.append(text)
+#list_of_rows.append(list_of_cells)
+#print(list_of_rows)
 
 
 
@@ -42,25 +52,3 @@ print(list_of_rows)
 
 
 
-#print(list_of_links)
-
-        
-        
-        #if a.find_all('href'):
-            #print(href)
-        
-        #print(a)
-
-
-
-#if li.find('a'):
-        #list_of_li.append("https://www.greenbeltmd.gov" + li.find('a')['href'])
-
-
-
-    #list_of_cells = []
-    #for a in li.find_all('a'):
-        #print('a')
-
-
-# to run python scraper/scrape.py
