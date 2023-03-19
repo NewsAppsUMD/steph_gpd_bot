e694cf0e3bd61672d245fd5ed458a54a7436edb7
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-03-19 17:44:12 +0000

    latest code

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -10,7 +10,6 @@ html = response.content
 soup = BeautifulSoup(html, features="html.parser")
 ul = soup.find_all('ul')[-1]
 
-list_of_rows = []
 for li in ul.find_all('li'):
     for a in li.find_all('a'):
         list_of_dates = []
@@ -20,44 +19,9 @@ for li in ul.find_all('li'):
         if li.find('a'):
             list_of_urls.append("https://www.greenbeltmd.gov" + li.find('a')['href'])
             list_of_dates.append(list_of_urls)
-list_of_rows.append(list_of_dates)
-print(list_of_rows)
-
-#outfile = open('./reports.csv', 'w')
-#writer = csv.writer(outfile)
-#writer.writerows(list_of_rows)
-
-#list_of_rows = []
-#for row in ul.find_all('li'):
-    #list_of_cells = []
-    #for cell in row.find_all('a'):
-        #if cell.find('a'):
-            #list_of_cells.append("https://www.greenbeltmd.gov" + cell.find('a')['href'])
-    #print(list_of_cells)
-        #if cell.find(text)
-            #list_of_cells.append(cell.text)
-    #text = cell.text.strip()
-    #list_of_cells.append(text)
-#list_of_rows.append(list_of_cells)
-#print(list_of_rows)
-
-
-
-
-
-
-       
-
-
-
-    
-    
         
-    
-    
-
-
-
-
-
+            
+        outfile = open('./reports.csv', 'w')
+        writer = csv.writer(outfile)
+        writer.writerows(list_of_dates)
 
