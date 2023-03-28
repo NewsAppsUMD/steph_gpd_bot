ccdf764bdef6a1ea0c3a362ca4c34c5055d562c5
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-03-28 00:37:18 +0000

    tabula to read latest pdf and convert to csv

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import tabula
 
 url = 'https://www.greenbeltmd.gov/i-want-to/view/weekly-crime-report/-folder-1474'
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -16,7 +17,7 @@ list_of_rows = []
 for li in ul.find_all('li'):
     #find the a tags, which is where the info is
     for a in li.find_all('a'):
-        #set up a contain for the date and url that will go in each row
+        #set up a container for the date and url that will go in each row
         each_row = []
         if li.find('a'):
             #in cases where there is an a tag in the list item, put together a url for the href inside the a tag and also print out the text in that a tag (which is the date)
@@ -30,5 +31,12 @@ for li in ul.find_all('li'):
 #broadly speaking, I know this creates a reports.csv file that puts the info from list_of_rows in table form, but I'm not sure what each line does.            
 outfile = open('./reports.csv', 'w')
 writer = csv.writer(outfile)
+writer.writerow(["url", "date"])
 writer.writerows(list_of_rows)
 
+#print(list_of_rows[0])
+
+#read pdf of newest weekly crime report
+dfs = tabula.read_pdf(list_of_rows[0][0], pages = "all")
+
+tabula.convert_into((list_of_rows[0][0]), 'output.csv', pages = 'all')
