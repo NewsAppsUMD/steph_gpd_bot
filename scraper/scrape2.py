087df76dcee548e6b0f7e7693fc37858855eafb2
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-03-31 16:07:29 +0000

    making list of dictionaries

--- scraper/scrape2.py
+++ scraper/scrape2.py
@@ -3,6 +3,7 @@ import csv
 import tabula
 import sqlite_utils
 import datasette_codespaces
+import requests
 
 url = 'https://www.greenbeltmd.gov/i-want-to/view/weekly-crime-report/-folder-1474'
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -25,12 +26,21 @@ for li in ul.find_all('li'):
             #appending the elements described in above note to each_row
             each_row.append("https://www.greenbeltmd.gov" + li.find('a')['href'])
             each_row.append(li.text)
-
+    list_of_rows.append(each_row)
+#print(list_of_rows)
+outfile = open('./reports.csv', 'w')
+writer = csv.writer(outfile)
 writer.writerow(["url", "date"])
 writer.writerows(list_of_rows)
 
-data=[]
-for row in list_of_rows:
-    df = tabula.read_pdf(list_of_rows, pages="all")
-    data.append(df)
-print(data)
+dictionaries=[]
+for i in range(0, len(list_of_rows), 2):
+    dictionaries.append({'url':list_of_rows[i], 'date':list_of_rows[i+1]})
+print(dictionaries)
+
+
+#data=[]
+#for each in list_of_rows:
+    #df = tabula.read_pdf(each['url'], pages="all")
+    #data.append(df)
+#print(data)
