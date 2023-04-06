3ce5d38ba42ff62ad55ca45b33f67fd9b6c5c0a5
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-04-06 19:33:25 +0000

    setup file

--- gpd_bot_setup.sh
+++ gpd_bot_setup.sh
@@ -1,2 +1,2 @@
-pip install requests bs4 tabula-py sqlite-utils datasette_codespaces slack os slack.errors
+pip install requests bs4 tabula-py sqlite-utils datasette_codespaces slack
 # to run: bash gpd_bot_setup.sh
