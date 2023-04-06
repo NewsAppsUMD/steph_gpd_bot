b9a841afbcc6a6d459a2704a8c4de653d43e2df0
stephquinn <113057116+stephquinn@users.noreply.github.com>
2023-04-06 19:22:54 +0000

    do i need slack.errors?

--- gpd_bot_setup.sh
+++ gpd_bot_setup.sh
@@ -1,2 +1,2 @@
-pip install requests bs4 tabula-py sqlite-utils datasette_codespaces slack os
+pip install requests bs4 tabula-py sqlite-utils datasette_codespaces slack os slack.errors
 # to run: bash gpd_bot_setup.sh
